c35cd0882af0c9eab26b053d7ece5d1f3025ad0e
Penilandia <penilandiaa@gmail.com>
2025-10-23 16:19:00 -0300

    Proyecto limpio listo para Vercel

--- src/components/ClientsGallery.js
+++ src/components/ClientsGallery.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 const clients = [
-  { id: 1, img: "/Clientes/reseña 1.jpeg" },
-  { id: 2, img: "/Clientes/reseña 2.jpeg" },
-  { id: 3, img: "/Clientes/reseña 3.jpeg" },
-  { id: 4, img: "/Clientes/reseña 4.jpeg" },
-  { id: 5, img: "/Clientes/reseña 5.jpeg" },
-  { id: 6, img: "/Clientes/reseña 6.jpeg" },
-  { id: 7, img: "/Clientes/reseña 7.jpeg" },
-  { id: 8, img: "/Clientes/reseña 8.jpeg" },
-  { id: 9, img: "/Clientes/reseña 9.jpeg" },
-  { id: 10, img: "/Clientes/reseña 10.jpeg" },
-  { id: 11, img: "/Clientes/reseña 11.jpeg" },
-  { id: 12, img: "/Clientes/reseña 12.jpeg" },
+  { id: 1, img: "/Clientes/Reseña1.jpeg" },
+  { id: 2, img: "/Clientes/Reseña2.jpeg" },
+  { id: 3, img: "/Clientes/Reseña3.jpeg" },
+  { id: 4, img: "/Clientes/Reseña4.jpeg" },
+  { id: 5, img: "/Clientes/Reseña5.jpeg" },
+  { id: 6, img: "/Clientes/Reseña6.jpeg" },
+  { id: 7, img: "/Clientes/Reseña7.jpeg" },
+  { id: 8, img: "/Clientes/Reseña8.jpeg" },
+  { id: 9, img: "/Clientes/Reseña9.jpeg" },
+  { id: 10, img: "/Clientes/Reseña10.jpeg" },
+  { id: 11, img: "/Clientes/Reseña11.jpeg" },
+  { id: 12, img: "/Clientes/Reseña12.jpeg" },
 ];
 
 export default function ClientsGallery() {
