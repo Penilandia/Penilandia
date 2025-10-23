84b131baf80993d522119165340fcd0699509822
Penilandia <penilandiaa@gmail.com>
2025-10-23 13:15:18 -0300

    Cambios de precios

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const products = [
-  { id: 1, name: "Camita naranja con rayas", image: "/Productos/cama 2.jpeg", price: "$12.500" },
-  { id: 2, name: "Camita azul con hueso amarillo", image: "/Productos/cama 7.jpeg", price: "$11.000" },
-  { id: 3, name: "Sillon de viaje con cinturon de seguridad", image: "/Productos/cama de viaje con cinturon de seguridad.jpeg", price: "$15.000" },
-  { id: 4, name: "Camita naranja con perritos", image: "/Productos/cama 4.jpeg", price: "$12.500" },
-  { id: 5, name: "Camita verde con ojas", image: "/Productos/cama 6.jpeg", price: "$13.000" },
-  { id: 6, name: "Camita de colores", image: "/Productos/cama 3.jpeg", price: "$11.800" },
-  { id: 7, name: "Gorro para el sol", image: "/Productos/gorros.jpeg", price: "$5.000" },
-  { id: 8, name: "Camita rosa", image: "/Productos/cama 5.jpeg", price: "$11.200" },
+  { id: 1, name: "Camita naranja con rayas", image: "/Productos/cama 2.jpeg", price: "$18.000" },
+  { id: 2, name: "Camita azul con hueso amarillo", image: "/Productos/cama 7.jpeg", price: "$28.000" },
+  { id: 3, name: "Sillon de viaje con cinturon de seguridad", image: "/Productos/cama de viaje con cinturon de seguridad.jpeg", price: "$35.000" },
+  { id: 4, name: "Camita naranja con perritos", image: "/Productos/cama 4.jpeg", price: "$20.000" },
+  { id: 5, name: "Camita verde con ojas", image: "/Productos/cama 6.jpeg", price: "$18.000" },
+  { id: 6, name: "Camita de colores", image: "/Productos/cama 3.jpeg", price: "$40.800" },
+  { id: 7, name: "Gorro para el sol", image: "/Productos/gorros.jpeg", price: "$10.000" },
+  { id: 8, name: "Camita rosa", image: "/Productos/cama 5.jpeg", price: "$37.000" },
 ];
 
 function ProductCard({ product }) {
